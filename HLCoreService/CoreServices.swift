6a999e8d00846f7389bd9db458f3559973866aca
Havi.X.Li.-ND@disney.com <>
2018-11-13 10:48:07 +0800

    new

---
+++ HLCoreService/CoreServices.swift
@@ -0,0 +1,16 @@
+//
+//  CoreServices.swift
+//  HLCoreService
+//
+//  Created by Li, Havi X. -ND on 2018/11/13.
+//  Copyright © 2018 Li, Havi X. -ND. All rights reserved.
+//
+
+import Foundation
+
+class CoreServices: NSObject {
+	
+	static func coreServicesResourceBundle() -> Bundle? {
+		return nil
+	}
+}
---
+++ HLCoreService/HLCoreService.h
@@ -0,0 +1,19 @@
+//
+//  HLCoreService.h
+//  HLCoreService
+//
+//  Created by Li, Havi X. -ND on 2018/11/13.
+//  Copyright © 2018 Li, Havi X. -ND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HLCoreService.
+FOUNDATION_EXPORT double HLCoreServiceVersionNumber;
+
+//! Project version string for HLCoreService.
+FOUNDATION_EXPORT const unsigned char HLCoreServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HLCoreService/PublicHeader.h>
+
+
